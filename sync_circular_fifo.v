87267ccd481175ddb7de2e23b465f69718fe23ea
Purvesh Sane <purvesh96@gmail.com>
2021-01-17 02:08:36 -0500

    Update sync_circular_fifo.v

--- sync_circular_fifo.v
+++ sync_circular_fifo.v
@@ -1,12 +1,39 @@
 module sync_circular_fifo
-	#(parameter DATA_WIDTH = 64, parameter NUM_FIFO_BLOCKS = 1024)
+	#(parameter DATA_WIDTH = 64, parameter NUM_FIFO_BLOCKS = 1024, parameter POINTER_NUM_BITS = $sqrt(NUM_FIFO_BLOCKS))
 	(input [(DATA_WIDTH - 1) : 0] data_i, wr_en_i, clk_i, rst_n_i,
-	 output full_o,
+	 output reg full_o,
 	 input rd_en_i,
-	 output [(DATA_WIDTH - 1) : 0] data_o, empty_o);
+	 output reg [(DATA_WIDTH - 1) : 0] data_o, empty_o);
 
-	always @ (posedge clk_i)
-		begin
-		
-		end
+	reg [(DATA_WIDTH - 1) : 0] fifo_data [(NUM_FIFO_BLOCKS - 1) : 0];
+	reg [POINTER_NUM_BITS : 0] head_pointer = 0; // Intentionally allowed one extra bit
+	reg [POINTER_NUM_BITS : 0] tail_pointer = 0; // Intentionally allowed one extra bit
+
+	reg [2:0] FSM_STATE = 3'b000;
+	parameter WRITE_TOGGLE_CHECK = 3'b000;
+	parameter WRITE_DENIED_FIFO_FULL = 3'b001;
+	parameter WRITE_DATA_I_TO_FIFO = 3'b010;
+
+	always @ (posedge clk_i) begin
+		case(FSM_STATE)
+			WRITE_TOGGLE_CHECK :
+				if (wr_en_i == 1'b1) begin
+					if (head_pointer == tail_pointer) begin
+						FSM_STATE = WRITE_DENIED_FIFO_FULL;
+					end
+					else begin
+						FSM_STATE = WRITE_DATA_I_TO_FIFO;
+					end
+				end
+			WRITE_DENIED_FIFO_FULL : begin
+				full_o = 1'b1;
+				FSM_STATE = WRITE_TOGGLE_CHECK;
+				end
+			WRITE_DATA_I_TO_FIFO : begin
+				fifo_data[tail_pointer] = data_i;
+				tail_pointer = tail_pointer + 1;
+				end
+			default : $display("Your FSM is Screwed Up!");
+		endcase
+	end
 endmodule
