33fb9f70ac163319cb92f22afa77d9ab805a86a7
Purvesh Sane <purvesh96@gmail.com>
2021-01-17 21:55:38 -0500

    Removed $sqrt and used $clog2 for POINTER_NUM_BITS as it is what makes sense.

--- sync_circular_fifo.v
+++ sync_circular_fifo.v
@@ -1,5 +1,5 @@
 module sync_circular_fifo
-	#(parameter DATA_WIDTH = 64, parameter NUM_FIFO_BLOCKS = 1024, parameter POINTER_NUM_BITS = $sqrt(NUM_FIFO_BLOCKS))
+	#(parameter DATA_WIDTH = 64, parameter NUM_FIFO_BLOCKS = 1024, parameter POINTER_NUM_BITS = $clog2(NUM_FIFO_BLOCKS))
 	(input [(DATA_WIDTH - 1) : 0] data_i, wr_en_i, clk_i, rst_n_i,
 	 output reg full_o,
 	 input rd_en_i,
@@ -11,29 +11,4 @@ module sync_circular_fifo
 
 	reg [2:0] FSM_STATE = 3'b000;
 	parameter WRITE_TOGGLE_CHECK = 3'b000;
-	parameter WRITE_DENIED_FIFO_FULL = 3'b001;
-	parameter WRITE_DATA_I_TO_FIFO = 3'b010;
-
-	always @ (posedge clk_i) begin
-		case(FSM_STATE)
-			WRITE_TOGGLE_CHECK :
-				if (wr_en_i == 1'b1) begin
-					if (head_pointer == tail_pointer) begin
-						FSM_STATE = WRITE_DENIED_FIFO_FULL;
-					end
-					else begin
-						FSM_STATE = WRITE_DATA_I_TO_FIFO;
-					end
-				end
-			WRITE_DENIED_FIFO_FULL : begin
-				full_o = 1'b1;
-				FSM_STATE = WRITE_TOGGLE_CHECK;
-				end
-			WRITE_DATA_I_TO_FIFO : begin
-				fifo_data[tail_pointer] = data_i;
-				tail_pointer = tail_pointer + 1;
-				end
-			default : $display("Your FSM is Screwed Up!");
-		endcase
-	end
-endmodule
+	parameter WRITE_DENIED_FI
